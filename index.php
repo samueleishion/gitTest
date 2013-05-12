56c2cc69d2e7524e4818d1ff4788f49d241d0dcd
Sam Acuna <samueleishion@gmail.com>
2013-05-12 16:48:42 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?
+
+$out = "Hello world"; 
+
+?>
+<html><head>
+ <link rel="stylesheet" type="text/css" href="_stys/global.css">
+ <title></title>
+</head><body>
+
+<? echo $out; ?>
+
+</body></html>
