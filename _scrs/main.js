f805f5b62e06361bd315b860cc4637ce27d43afd
Sam Acuna <samueleishion@gmail.com>
2013-05-12 18:21:28 -0500

    adding scripts

---
+++ _scrs/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$('.hw a').click(function(e){
+		e.preventDefault(); 
+	}); 
+}); 
--- index.php
+++ index.php
@@ -5,11 +5,13 @@ $out = "Hello world!";
 ?>
 <html><head>
  <link rel="stylesheet" type="text/css" href="_stys/global.css">
- <title></title>
+ <script type="text/javascript" src="_scrs/jquery-1.7.min.js"></script>
+ <script type="text/javascript" src="_scrs/main.js"></script>
+ <title>Hello Git-World!</title>
 </head><body>
 
 <div class="hw">
- <? echo $out; ?>
+ <a href="#"><? echo $out; ?></a>
 </div>
 
 </body></html>
